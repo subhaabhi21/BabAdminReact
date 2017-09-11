08305abb475923ed60bbb55b8e0c29f5652ae512
subha <subhasree.g@infibeam.net>
2017-09-11 16:33:04 +0530

    Removed unwanted files

--- README.md
+++
@@ -1,2 +0,0 @@
-# BabAdminReact
-BabAdmin- ReactNative
--- adminapp/components/app_common/common.js
+++
@@ -1,7 +0,0 @@
-var api = {
-	authenticate(url,fetch_type){		
-    return fetch(url,{method: fetch_type}).then((response) => response.json())
-	}
-}
-
-module.exports = api;
