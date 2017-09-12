4f548fe740b72138e0cf2c87320ddec232aee5df
subha <subhasree.g@infibeam.net>
2017-09-12 11:22:46 +0530

    backup due to project errors

---
+++ Dummy/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+          AppRegistry,
+          StyleSheet,
+          View,
+          ActivityIndicator,
+       } from 'react-native';
+
+import {
+ StackNavigator,
+} from 'react-navigation';
+import LoginScreen from './components/Login/login';
+
+import HomeScreen from './components/Login/home';
+import PurchasesScreen from './components/Purchase/purchases';
+import SearchScreen from './components/Search/search';
+
+
+const AdminApp = StackNavigator({
+  Login: { screen: LoginScreen },
+  Search : {screen : SearchScreen},
+  Home: { screen: HomeScreen },
+  Purchases: { screen: PurchasesScreen }
+  },
+  {
+    mode:'card',
+    headerMode: 'float'
+  }
+);
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false
+    };
+
+  }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+    });
+
+    this.setState({ isReady: true });
+  }
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <View style={{flex: 1, justifyContent: 'center'}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
+    return (
+      <AdminApp />
+   )
+  }
+}
---
+++ Dummy/components/Purchase/purchases.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import {
+					AppRegistry,
+					StyleSheet,
+					View,
+					TextInput,
+					ActivityIndicator,
+					KeyboardAvoidingView,
+					FlatList,
+			} from 'react-native';
+
+import Api from '../app_common/common';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base'
+
+class PurchasesScreen extends React.Component{
+	static navigationOptions = {
+	 title: 'Purchases',
+ 	};
+
+	constructor() {
+			console.log("inn constructor of Purchase")
+		super();
+
+		this.state = {
+      purchases : []
+		};
+		this.getPurchases()
+	}
+
+
+	getPurchases(){
+		Api.call_api("purchases/get_purchases.json","","GET").then((response) => {
+			if(response.status == 'success'){
+        this.setState({purchases : response.result})
+			}
+			else{
+				if(response.error)
+					alert(response.error[0]);
+			}
+		});
+	}
+
+  _keyExtractor = (item, index) => item.number;
+
+	render(){
+    if (Api.isLoading) {
+       return (
+				 <Container>
+					 <Header>
+		         <Left>
+		           <Button transparent>
+		             <Icon name='menu' />
+		           </Button>
+		         </Left>
+		         <Body>
+		           <Title>Header</Title>
+		         </Body>
+		         <Right />
+		       </Header>
+	       	<Content padder>
+         <View style={{flex: 1, justifyContent: 'center'}}>
+           <ActivityIndicator />
+         </View>
+			 </Content>
+		 	</Container>
+			)
+    }
+
+     return (
+
+			 <Container>
+				 <Header>
+	         <Left>
+	           <Button transparent>
+	             <Icon name='menu' />
+	           </Button>
+	         </Left>
+	         <Body>
+	           <Title>Header</Title>
+	         </Body>
+	         <Right />
+	       </Header>
+       	<Content padder>
+		      <View style={styles.container}>
+		        <FlatList
+		          data={this.state.purchases}
+		          keyExtractor={this._keyExtractor}
+		          renderItem={({item}) =>
+		            <View style = {styles.purchase}>
+		              <Text >Purchase # : {item.number}</Text>
+		              <Text >Status : {item.status}</Text>
+		              <Text >Total : {item.total.value}</Text>
+		              <Text >Email : {item.email}</Text>
+		            </View>
+		          }
+		        />
+		      </View>
+					</Content>
+				</Container>
+    );
+	}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+  },
+  purchase: {
+    flex: 1,
+    backgroundColor: '#dedede',
+    marginBottom: 10,
+    padding:10
+
+  },
+	buttonContainer: {
+		backgroundColor: '#4286f4',
+		paddingVertical: 15
+	},
+	buttonText: {
+		textAlign: 'center',
+		color: '#ffffff'
+	}
+});
+
+module.exports = PurchasesScreen;
